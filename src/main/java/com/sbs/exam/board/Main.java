8ff89a7eb8700777b6b415e4c09ed52694dd6ce8
ahin2457 <dkdls2457@gmail.com>
2024-10-15 19:27:16 +0900

    명령어 입력받기

--- src/main/java/com/sbs/exam/board/Main.java
+++ src/main/java/com/sbs/exam/board/Main.java
@@ -1,10 +1,19 @@
 package com.sbs.exam.board;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-
+    Scanner sc = new Scanner(System.in);
     System.out.println("== 텍스트 게시판 v 0.1 ==");
     System.out.println("프로그램 시작");
+
+    System.out.printf("명령) ");
+    String cmd = sc.nextLine();
+
+    System.out.printf("입력받은 명령어: %s\n",cmd);
+
+
     System.out.println("프로그램 끝");
     System.out.println("== 게시판을 종료합니다 ==");
   }
