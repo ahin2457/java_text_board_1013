8318c44fa944decbca78aa634a6c9b2c7408c963
ahin2457 <dkdls2457@gmail.com>
2024-10-16 15:55:19 +0900

    exit 입력받기 전까지 명령어 입력 받기

--- src/main/java/com/sbs/exam/board/Main.java
+++ src/main/java/com/sbs/exam/board/Main.java
@@ -8,13 +8,25 @@ public class Main {
     System.out.println("== 텍스트 게시판 v 0.1 ==");
     System.out.println("프로그램 시작");
 
-    System.out.printf("명령) ");
-    String cmd = sc.nextLine();
+    while (true){
+      System.out.printf("명령) ");
+      String cmd = sc.nextLine();
 
-    System.out.printf("입력받은 명령어: %s\n",cmd);
+      if(cmd.equals("exit") ){
+        break; // 반복문 빠져나오기
+      }
+
+      System.out.printf("입력받은 명령어: %s\n",cmd);
+
+    }
+
+    System.out.println("== 자바 텍스트 게시판 종료 ==");
+
+    sc.close();
 
 
-    System.out.println("프로그램 끝");
-    System.out.println("== 게시판을 종료합니다 ==");
+
+    //System.out.println("프로그램 끝");
+    //System.out.println("== 게시판을 종료합니다 ==");
   }
 }
